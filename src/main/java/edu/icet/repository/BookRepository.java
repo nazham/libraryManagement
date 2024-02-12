197ca31c3c70af5ebc500dc5f20d6ab23ca9cacc
nazham <nazhammohamed@gmail.com>
2024-02-12 20:51:25 +0530

    addBook method implemented

---
+++ src/main/java/edu/icet/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package edu.icet.repository;
+
+import edu.icet.entity.BookEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<BookEntity,Long> {
+
+}
--- src/main/java/edu/icet/service/impl/BookServiceImpl.java
+++ src/main/java/edu/icet/service/impl/BookServiceImpl.java
@@ -1,13 +1,27 @@
 package edu.icet.service.impl;
 
 import edu.icet.dto.Book;
+import edu.icet.entity.BookEntity;
+import edu.icet.repository.BookRepository;
 import edu.icet.service.BookService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
+    @Autowired
+    BookRepository repository;
+
+    ModelMapper mapper;
+
+    @Bean
+    public void setup(){this.mapper = new ModelMapper(); }
+
     @Override
     public void addBook(Book book) {
-        //example code
+        BookEntity entity = mapper.map(book, BookEntity.class);
+        repository.save(entity);
     }
 }
